1267d1c458decfd9c6211b240a287d94300b0e99
Luigi Ishii <liluishii@gmail.com>
2023-05-12 12:27:37 -0300

    3

---
+++ fila/MaisUmTeste.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class MaisUmTeste {
+    public static void main(String[] args) {
+        Random r = new Random();
+        FilaDinamica f = new FilaDinamica();
+        do{
+            if(r.nextInt(2) ==0 ){//enfileirar
+                f.enfilera(r.nextInt(10));
+            }
+            else{ //desenfileira
+                if(!f.estaVazia())
+                System.out.println(f.desenfilera() + " foi atendido");
+            }
+            System.out.println(f);
+        }  while(!f.estaVazia());
+    }
+}
