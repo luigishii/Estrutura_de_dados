9af702df36cec01c517712fab08d7feac57260e0
Luigi Ishii <liluishii@gmail.com>
2023-05-12 12:56:52 -0300

    oi

--- Listas_Genericas/No.java
+++ Listas_Genericas/No.java
@@ -8,4 +8,21 @@ public class No<T> {
         this.info = info;
         this.prox = null;
     }
+
+    public T getInfo() {
+        return info;
+    }
+
+    public void setInfo(T info) {
+        this.info = info;
+    }
+
+    public No<T> getProx() {
+        return prox;
+    }
+
+    public void setProx(No<T> prox) {
+        this.prox = prox;
+    }
+    
 }
