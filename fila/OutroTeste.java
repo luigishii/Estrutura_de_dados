ba2c4b19ac31c6d7813e93aee5027b57d5d398d9
Luigi Ishii <126471025+luigishii@users.noreply.github.com>
2023-05-12 10:35:28 -0300

    Add files via upload

---
+++ fila/OutroTeste.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class OutroTeste {
+    public static void main(String[] args) {
+        Random r = new Random();
+        FilaEstatica f = new FilaEstatica(12);
+        if(r.nextInt(2) ==0 ){//enfileirar
+            if(!f.estaCheia())
+            f.enfilera(r.nextInt(10));
+        }
+        else{ //desenfileira
+            if(!f.estaVazia())
+            System.out.println(f.desenfilera() + " foi atendido");
+        }
+    }    
+}
