292cc9b3107ef128f727046f43e0746fb38d7eae
Luigi Ishii <126471025+luigishii@users.noreply.github.com>
2023-05-12 10:30:04 -0300

    Add files via upload

---
+++ fila/TesteFila.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class TesteFila {
+    
+    public static void main(String[] args) {
+        FilaEstatica f = new FilaEstatica();
+        System.out.println(f);
+        Random r = new Random();
+        while (!f.estaCheia()) {
+            f.enfilera(r.nextInt(0, 9));
+            System.out.println(f);
+        }
+        while(!f.estaVazia()) {
+            System.out.println(f.desenfilera() + " foi atendido");
+        }
+    }
+
+}
