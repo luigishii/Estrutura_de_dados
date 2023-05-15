44e3ebd55ec162ec5b92f0e6cd4865d87deaa006
luigishii <liluishii@gmail.com>
2023-05-15 16:35:24 -0300

    estudando estrutura

--- fila/FilaEstatica.java
+++ fila/FilaEstatica.java
@@ -1,72 +1,100 @@
-public class FilaEstatica{
-    private int[] dados;
-    private int tamanho, prim, ult;
-    public FilaEstatica(){
-        dados = new int[10];
-        prim = 0;ult = 0; tamanho=0;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class FilaEstatica {
+    private int[] dados; //valores no vetor
+    private int tamanho,
+    prim,
+    ult; //tamanho do vetor, primeiro e ultimo valor
+    public FilaEstatica() {
+        dados = new int[10]; //vetor tem 10 valores
+        prim = 0;
+        ult = 0;
+        tamanho = 0;
     }
-    public FilaEstatica(int capacidade){
-        dados = new int[capacidade];
-        prim=0;ult=0;tamanho=0;
+    public FilaEstatica(int capacidade) {
+        dados = new int[capacidade]; //dá outro valor à capacidade do vetor
+        prim = 0;
+        ult = 0;
+        tamanho = 0;
     }
-    public boolean estaVazia(){
-        return tamanho == 0;
+    public boolean estaVazia() {
+        return tamanho == 0; // se a lista está vazia ele retorna 0
     }
-    public boolean estaCheia(){
-        return tamanho == dados.length;
+    public boolean estaCheia() {
+        return tamanho == dados.length; //se a lista está cheia retorna o tamanho dos dados
     }
-    int proxima(int pos){
-        return (pos + 1) % dados.length;
+    int proxima(int pos) {
+        return (pos + 1) % dados.length; //proxima retorna o valor da posicao mais 1
     }
-    public void enfilera(int i){
+    public void enfilera(int i) {
         dados[ult] = i;
         ult = proxima(ult);
-        tamanho ++;
+        tamanho++;
     }
-    public int desenfilera(){
+    public int desenfilera() {
         int i = dados[prim];
         prim = proxima(prim);
         tamanho--;
         return i;
     }
-    @Override
-    public String toString(){
-        String s = "fila: ";
-        if(estaVazia()){
-            s += "esta vazia";
+
+    public static Queue<Integer> mergeQueues(
+        Queue<Integer> dados1,
+        Queue<Integer> dados2
+    ) {
+        Queue<Integer> resultado = new LinkedList<Integer>();
+
+        while (
+            !((FilaEstatica)dados1).estaVazia() || !((FilaEstatica)dados2).estaVazia()
+        ) {
+            if (!((FilaEstatica)dados1).estaVazia()) {
+                resultado.offer(dados2.poll());
+            }
+            if (!((FilaEstatica)dados2).estaVazia()) {
+                resultado.offer(dados2.poll());
+            }
         }
-        else{
+        return resultado;
+    }
+
+    @Override public String toString() {
+        String s = "fila: ";
+        if (estaVazia()) {
+            s += "esta vazia";
+        } else {
             int i = prim;
-            do{
+            do {
                 s += dados[i] + " ";
                 i = proxima(i);
-            }while(i != ult);
+            } while (i != ult);
         }
         return s;
     }
-    public String oVetor(){
+    public String oVetor() {
         String s = "";
-        if (estaVazia())
-            for(int i=0; i<dados.length;i++)
-                s+= "_ ";
-        else if(estaCheia())
-            for (int i=0; i<dados.length;i++)
-                s+= dados[i] + " ";
-        else if (prim<ult) {
-            for(int i = 0; i<prim; i++)
-                s+= "_ ";
-            for(int i=prim; i<ult;i++)
-                s+= dados[i] + " ";
-            for(int i = ult;i<dados.length; i++)
-                s+= "_";
-    }else {//prim > ult}
-        for(int i=0;i<ult ; i++)
-            s+= dados[i] + " ";
-        for(int i = ult; i<prim; i++)
-            s+="_ ";
-        for (int i=prim; i<dados.length; i++)
-            s+= dados[i] + " "; 
-    }
+        if (estaVazia()) 
+            for (int i = 0; i < dados.length; i++) 
+                s += "_ ";
+            else if (estaCheia()) 
+                for (int i = 0; i < dados.length; i++) 
+                    s += dados[i] + " ";
+    else if (prim < ult) {
+            for (int i = 0; i < prim; i++) 
+                s += "_ ";
+            for (int i = prim; i < ult; i++) 
+                s += dados[i] + " ";
+            for (int i = ult; i < dados.length; i++) 
+                s += "_";
+            }
+        else { //prim > ult}
+            for (int i = 0; i < ult; i++) 
+                s += dados[i] + " ";
+            for (int i = ult; i < prim; i++) 
+                s += "_ ";
+            for (int i = prim; i < dados.length; i++) 
+                s += dados[i] + " ";
+            }
         return s;
-}
+    }
 }
---
+++ fila/TesteMergeFilas.java
@@ -0,0 +1,48 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TesteMergeFilas {
+    public static Queue<Integer> mergeQueues(
+        Queue<Integer> queue1,
+        Queue<Integer> queue2
+    ) {
+        Queue<Integer> mergedQueue = new LinkedList<>();
+
+        while (!queue1.isEmpty() || !queue2.isEmpty()) {
+            if (!queue1.isEmpty()) {
+                mergedQueue.offer(queue1.poll());
+            }
+            if (!queue2.isEmpty()) {
+                mergedQueue.offer(queue2.poll());
+            }
+        }
+
+        return mergedQueue;
+    }
+
+    public static void main(String[] args) {
+        Queue<Integer> dados1 = new LinkedList<>();
+        dados1.offer(1);
+        dados1.offer(3);
+        dados1.offer(5);
+        dados1.offer(7);
+        dados1.offer(9);
+
+        Queue<Integer> dados2 = new LinkedList<>();
+        dados2.offer(2);
+        dados2.offer(4);
+        dados2.offer(6);
+        dados2.offer(8);
+        dados2.offer(10);
+        dados2.offer(12);
+        dados2.offer(14);
+
+        Queue<Integer> resultado = mergeQueues(dados1, dados2);
+
+        while (!resultado.isEmpty()) {
+            System
+                .out
+                .print(resultado.poll() + " ");
+        }
+    }
+}
