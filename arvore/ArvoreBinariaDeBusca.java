a036bdee089b62282dc92c2d40ba7044f8457550
LUIGI KENZO ISHII <22.01390-3@maua.br>
2023-06-02 10:27:42 -0300

    olá

--- arvore/ArvoreBinariaDeBusca.java
+++ arvore/ArvoreBinariaDeBusca.java
@@ -1,9 +1,5 @@
 package arvore;
 
-
-
-
-
 public class ArvoreBinariaDeBusca {
     private class NoBin { // NoBin = Nó Binário
         private int info;
@@ -50,49 +46,49 @@ public class ArvoreBinariaDeBusca {
     //só o construtor padrão
     
     public void insere(int i){
-        NoBin novo = new NoBin(i);
-        if(raiz == null){
-            raiz = novo;
+        NoBin novo = new NoBin(i); //disponibiliza um novo nó para carregar o número
+        if(raiz == null){ //se a raiz está vazia 
+            raiz = novo; //coloca o valor que está no nó(novo) no lugar da raiz
         }
         else{
-            insereRec(raiz, novo);
+            insereRec(raiz, novo); //o novo valor é colocado na Direita ou na esquerda de acordo com o valor da raiz
         }
     }
 
     private void insereRec(NoBin atual, NoBin novo){
-        if(novo.getInfo() <= atual.getInfo()){
-            if(atual.getEsq()==null){
-                atual.setEsq(novo);
+        if(novo.getInfo() <= atual.getInfo()){ //se o novo valor for <= ao valor atual(raiz)
+            if(atual.getEsq()==null){   //se o valor à esquerda da raiz está vazia
+                atual.setEsq(novo); // coloca o novo valor na esquerda
             }
             else{
-                insereRec(atual.getEsq(), novo);
+                insereRec(atual.getEsq(), novo); //se o valor à esquerda da raiz está cheio, repete o método e o aplica denovo
             }
         }
-        else{
-            if(atual.getDir()==null){
-                atual.setDir(novo);
+        else{ //se o novo valor for > que o valor atual(raiz)
+            if(atual.getDir()==null){ //se nao tem nenhum numero à direita da raiz
+                atual.setDir(novo); // coloca o novo valor na direita da raiz
             }
-            else{
-                insereRec(atual.getDir(), novo);
+            else{ //se existe algum número na direita da raiz
+                insereRec(atual.getDir(), novo); //se o valor à direita da raiz está cheio, repete o método e o aplica denovo
             }
         }
     }
     
     public String exibeEmOrdem(){
 
-        if(raiz == null) return "árvore vazia";
+        if(raiz == null) return "árvore vazia"; // se nao tem raiz é pq a arvore esta vazia
 
-        else{
-            return exibeEmOrdemRec(raiz);
+        else{ // existe um valor na raiz
+            return exibeEmOrdemRec(raiz); 
         }
     }
 
-    private String exibeEmOrdemRec(NoBin atual){
-        if(atual != null){
-            return exibeEmOrdemRec(atual.getEsq()) + (atual.getInfo()) + exibeEmOrdemRec(atual.getDir());
+    private String exibeEmOrdemRec(NoBin atual){ // recebe o valor da raiz
+        if(atual != null){ // se há valor na raiz
+            return exibeEmOrdemRec(atual.getEsq()) + (atual.getInfo()) + exibeEmOrdemRec(atual.getDir()); //retorna todos os numeros a esquerda da raiz + a  raiz + todos os numeros a direita da raiz
         }       
         else{
-            return " ";
+            return " "; // se não há valor na raiz retorna um espaço vazio
        }
     }
     
@@ -114,20 +110,20 @@ public class ArvoreBinariaDeBusca {
        }
     }
 
-    public int contaNosPosOrdem(){
-        if(raiz == null) {
-            return 0;
+    public int contaNosPosOrdem(){ // metodo para contar os nós 
+        if(raiz == null) {// se não há valor na raiz 
+            return 0; //retorna 0
         }
-        else {
-            return 0;
+        else { // se há valor na raiz
+            return contaNosPosOrdemRec(raiz);
         }
     }
     public int contaNosPosOrdemRec(NoBin atual){
-        if(atual != null){
-            return contaNosPosOrdemRec(atual.getEsq()) + (contaNosPosOrdemRec(atual.getDir())) + 1;
+        if(atual != null){ // se há valor na raiz
+            return contaNosPosOrdemRec(atual.getEsq()) + (contaNosPosOrdemRec(atual.getDir())) + 1; //retorna o numero de nós a esquerda da raiz mais o numero de nós à direita da raiz + 1
         }
-        else{
-            return 0;
+        else{ // se não há valor na raiz 
+            return 0; //retorna 0
         }
     }
 }
