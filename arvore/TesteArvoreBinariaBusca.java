913f84647486253080a597e7b003bda8c52ad16c
LUIGI KENZO ISHII <22.01390-3@maua.br>
2023-06-02 12:08:54 -0300

    ola

--- arvore/TesteArvoreBinariaBusca.java
+++ arvore/TesteArvoreBinariaBusca.java
@@ -10,7 +10,7 @@ public class TesteArvoreBinariaBusca {
     public static void main(String[] args) {
         Random r = new Random(); // atribui um valor aleatório a r
         ArvoreBinariaDeBusca abb = new ArvoreBinariaDeBusca(); // abb assume o valor de ArvoreBinariaDeBusca atribuindo a ela um novo valor
-        for(int i=1; i<=10; i++){ // comecando em i=1 , enquanto i<=10 valores , adiciona 1 a i; 
+        for(int i=1; i<=10; i++){ // comecando em i=1 , enquanto i<=10 valores , ele corre pela lista; 
             int n = r.nextInt(50); // n assume o valor de um r tem um raio de até 50
             System.out.print(n + " ");
             abb.insere(n); // abb usa o metodo de inserir para inserir o valor n na lista  
